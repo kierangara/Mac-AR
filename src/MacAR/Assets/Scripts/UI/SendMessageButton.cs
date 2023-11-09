450077551371d5f84e01f4bf4511cb4c3acec7d2
Kieran Gara <garak@mcmaster.ca>
2023-11-09 02:43:05 -0500

    Fixed UI scaling

---
+++ src/MacAR/Assets/Scripts/UI/SendMessageButton.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+using UnityEngine.UI;
+
+public class SendMessageButton : MonoBehaviour
+{
+/*     public int maxMessages = 25;
+
+    public GameObject chatPanel, textObject;
+    public InputField chatBox;
+
+    [SerializeField]
+    List<Message> messageList = new List<Message>(); */
+	public void OnButtonPress()
+	{
+        Debug.Log("Button");
+		//VivoxVoiceManager.SendTextMessage();
+	}
+/*        public void SendMessageToChat(string text){
+        if(messageList.Count >= maxMessages){
+            Destroy(messageList[0].textObject.gameObject);
+            messageList.Remove(messageList[0]);
+        }
+
+        Message newMessage = new Message();
+        newMessage.text = text;
+
+        GameObject newText = Instantiate(textObject, chatPanel.transform);
+
+        newMessage.textObject = newText.GetComponent<Text>();
+        newMessage.textObject.text = newMessage.text;
+
+        messageList.Add(newMessage);
+    }
+    [System.Serializable]
+    public class Message{
+        public string text;
+        public Text textObject;
+    } */
+}
