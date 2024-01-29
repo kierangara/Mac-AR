f6348286c91751616c4f510ec68a8957e7c5d0bd
Kieran Gara <garak@mcmaster.ca>
2024-01-29 04:44:00 -0500

    Resolves issue #88 - voice chat toggle

--- src/MacAR/Assets/Scripts/UI/DisplayChat.cs
+++ src/MacAR/Assets/Scripts/UI/DisplayChat.cs
@@ -6,12 +6,37 @@ using UnityEngine.UI;
 public class DisplayChat : MonoBehaviour
 {
     CanvasGroup canvasGroup;
+    public Sprite textChatLogo;
+    public Sprite xLogo;
     // Start is called before the first frame update
     void Start()
     {
         canvasGroup = GameObject.Find("ChatWindow").GetComponent<CanvasGroup>();
-        canvasGroup.alpha = 1;
-        canvasGroup.interactable = true;
-        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void ToggleChat(){
+        if(canvasGroup.alpha == 0){
+            ShowChat();
+        }
+        else{
+            HideChat();
+        }
+    }
+    void ShowChat(){
+        if(canvasGroup.alpha == 0){
+            canvasGroup.alpha = 1;
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+            GameObject.Find("DisplayChat").GetComponent<Image>().overrideSprite = xLogo;
+        }
+    }
+
+    void HideChat(){
+        if(canvasGroup.alpha == 1){
+            canvasGroup.alpha = 0;
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+            GameObject.Find("DisplayChat").GetComponent<Image>().overrideSprite = textChatLogo;
+        }
     }
 }
