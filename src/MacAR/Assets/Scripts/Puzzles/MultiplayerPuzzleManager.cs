dc002e2b610ea805423c5fdf382e1c71f327d964
Sam <gormansam8@gmail.com>
2024-01-28 20:43:29 -0500

    Attempt at networked puzzle

--- src/MacAR/Assets/Scripts/Puzzles/MultiplayerPuzzleManager.cs
+++ src/MacAR/Assets/Scripts/Puzzles/MultiplayerPuzzleManager.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class MultiplayerPuzzleManager : MonoBehaviour
+public class MultiplayerPuzzleManager : NetworkBehaviour
 {
     // private List<GameObject> puzzles = new List<GameObject>();
-    public GameObject puzzle;
+    [SerializeField] private NetworkObject puzzle;
     public Camera cam; 
 
     //Start is called before the first frame update
     void Start()
     {
-        var puzzleInstance = Instantiate(puzzle, new Vector3(0, 0, 0), Quaternion.identity);
-        // Make Abstract
-        puzzleInstance.GetComponentInChildren<ObjectRotate>().cam = cam;
+        // // Register Prefabs
+        // NetworkManager.AddNetworkPrefab(puzzle);
+
+        // // Spawn Puzzle Instance
+        // var puzzleInstance = Instantiate(puzzle, new Vector3(0, 0, 0), Quaternion.identity);
+        // /// Make Abstract
+        // puzzleInstance.GetComponentInChildren<ObjectRotate>().cam = cam;
+        // puzzleInstance.GetComponentInChildren<NetworkObject>().Spawn();
+        SpawnPuzzleServerRpc();
+    }
+
+    [ServerRpc]
+    private void SpawnPuzzleServerRpc()
+    {
+       NetworkObject puzzleInstance = Instantiate(puzzle, new Vector3(0, 0, 0), Quaternion.identity); 
+       puzzleInstance.GetComponentInChildren<ObjectRotate>().cam = cam;
+       puzzleInstance.SpawnWithOwnership(OwnerClientId);
     }
 
     // Update is called once per frame
