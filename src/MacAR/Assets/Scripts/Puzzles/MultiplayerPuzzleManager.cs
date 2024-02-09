2361398186a021b000733b03a2cc7c28656658a5
Sam <gormansam8@gmail.com>
2024-02-08 22:07:01 -0500

    Fixed client list

--- src/MacAR/Assets/Scripts/Puzzles/MultiplayerPuzzleManager.cs
+++ src/MacAR/Assets/Scripts/Puzzles/MultiplayerPuzzleManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Unity.Netcode;
 
@@ -14,6 +16,7 @@ public class MultiplayerPuzzleManager : NetworkBehaviour
     void Start()
     {
         Debug.Log("Multiplayer Puzzle Manager called");
+
         SpawnPuzzleServerRpc();
     }
 
@@ -29,7 +32,24 @@ public class MultiplayerPuzzleManager : NetworkBehaviour
         {
             return;
         }
+        
         Debug.Log("SpawnPuzzleServerRpcCalled");
+
+        // Get Users
+        /// TODO: Exists here in case of changes to list, check if can be moved to start and updated asyncronously 
+        List<ulong> clients = new List<ulong>();
+        foreach(NetworkClient client in NetworkManager.Singleton.ConnectedClientsList)
+        {
+            clients.Add(client.ClientId);
+        }
+
+        Debug.Log("Init 0");
+
+        // Serialize
+        byte[] bytes = objectToBytes(clients);
+        Debug.Log($"[{string.Join(",", clients)}]");
+        Debug.Log($"[{string.Join(",", bytes)}]");
+
         // Instantiate 
         puzzleInstance = Instantiate(puzzle); 
 
@@ -37,25 +57,33 @@ public class MultiplayerPuzzleManager : NetworkBehaviour
         puzzleInstance.SpawnWithOwnership(OwnerClientId);
 
         // Initialize
-        InitializeClientRpc(puzzleInstance);
+        InitializeClientRpc(puzzleInstance, bytes);
     }
 
     [ClientRpc]
-    private void InitializeClientRpc(NetworkObjectReference puzzleRef)
+    private void InitializeClientRpc(NetworkObjectReference puzzleRef, byte[] clientBytes)
     {
+        Debug.Log("Init 1");
         if (puzzleRef.TryGet(out NetworkObject puzzle))
         {
+            // Deserialize
+            List<ulong> clients = bytesToObject(clientBytes);
+
+            Debug.Log("Init 2");
             puzzle.GetComponentInChildren<PuzzleData>().cam = cam;
             puzzle.GetComponentInChildren<PuzzleData>().completePuzzle = this;
 
-            foreach (NetworkClient client in NetworkManager.Singleton.ConnectedClientsList)
+            // TODO: Check if there are any ref issues with passing in entire list at once, faster than 
+            // one at a time
+            Debug.Log($"[{string.Join(",", clientBytes)}]");
+            Debug.Log($"[{string.Join(",", clients)}]");
+            foreach (ulong client in clients)
             {
-                puzzle.GetComponentInChildren<PuzzleData>().connectedClients.Add(client.ClientId);
+                puzzle.GetComponentInChildren<PuzzleData>().connectedClients.Add(client);
             }
             puzzle.GetComponentInChildren<MazePuzzle>().InitializePuzzle();
             //puzzleRef.
-
-
+            Debug.Log("Init 3");
         }
         
     }
@@ -77,5 +105,21 @@ public class MultiplayerPuzzleManager : NetworkBehaviour
     void Update()
     {
         
+    }
+
+    private byte[] objectToBytes(List<ulong> clients) 
+    {
+        return clients
+            .SelectMany(BitConverter.GetBytes)
+            .ToArray();
+    }
+
+    private List<ulong> bytesToObject(byte[] bytes) 
+    {
+        // TODO: Add ulong size check that changes Uint64/ UInt32
+        var size = sizeof(ulong);
+        return Enumerable.Range(0, bytes.Length / size)
+                     .Select(i => BitConverter.ToUInt64(bytes, i * size))
+                     .ToList();
     }
 }
