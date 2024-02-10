367ef6bc318fce5b373460d30d52613711edfa1a
Kieran Gara <garak@mcmaster.ca>
2024-02-10 16:50:17 -0500

    Puzzle Base working

--- src/MacAR/Assets/Scripts/Puzzles/CombinationPuzzle/CombinationPuzzle.cs
+++ src/MacAR/Assets/Scripts/Puzzles/CombinationPuzzle/CombinationPuzzle.cs
@@ -3,27 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using TMPro;
+using Unity.Netcode;
+using System.Linq;
 
-public class CombinationPuzzle : MonoBehaviour
+
+public class CombinationPuzzle : PuzzleBase
 {
     [SerializeField] TextMeshProUGUI codeInputField;
+    [SerializeField]  GameObject combinationPuzzle;
+    public PuzzleData puzzleData;
 
-    string[] codeCombo;
-    string currentCode;
-    int currentDigit;
+    private string[] codeCombo;
+    private string currentCode;
+    private int currentDigit;
     // Start is called before the first frame update
     void Start()
     {
+        //Read initial placeholder code
+        Debug.Log("Combo Puzzle Started");
         foreach(char j in codeInputField.text){
             currentCode+=j;
         }
         currentDigit = 0;
+        //Create code and instructions
         string code = "1359";
         string instr1 = "The second row and column each contain one number";
         string instr2 = "The second number is the only number in the first column";
         string instr3 = "The first number is two greater than the second number";
         string instr4 = "The fourth number is the only number in the third row";
         codeCombo = new string[]{code,instr1, instr2, instr3, instr4};
+    }
+
+    public override void InitializePuzzle(){
+        if (NetworkManager.Singleton.LocalClientId == puzzleData.connectedClients[0])
+        {
+
+        }
+        Debug.Log("COMBO PUZZLE INITIALIZED");
     }
 
     // Update is called once per frame
@@ -35,8 +51,12 @@ public class CombinationPuzzle : MonoBehaviour
     public void KeyPadPress(string number){
         string temp;
         Debug.Log($"Clicked {number}");
+        //Get digit from code to test input against
         char correctDigit = codeCombo[0][currentDigit];
+        //Check if input is correct digit, output to screen if correct and move on to next digit
+        Debug.Log("Test1");
         if(char.Parse(number)==correctDigit){
+            Debug.Log("Test3");
             if(currentDigit==0){
                 temp = number + currentCode.Substring(1,currentCode.Length-1);
             }
@@ -44,6 +64,7 @@ public class CombinationPuzzle : MonoBehaviour
                 temp = currentCode.Substring(0,currentDigit*2) + number 
                 + currentCode.Substring(currentDigit*2+1,currentCode.Length-(currentDigit*2+1));
             }
+            //Puzzle complete
             else{
                 temp = "Correct";
             }
@@ -52,10 +73,53 @@ public class CombinationPuzzle : MonoBehaviour
             codeInputField.text = temp;
             currentDigit+=1;
         }
+        //Incorrect digit entered, reset.
         else{
             currentDigit = 0;
             currentCode = "_ _ _ _";
             codeInputField.text = "_ _ _ _";
+            Debug.Log(currentCode);
+            Debug.Log(codeInputField.text);
         }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void RequestPuzzleDataServerRpc()
+    {
+        RequestPuzzleDataClientRpc();
+    }
+
+    [ClientRpc]
+    public void RequestPuzzleDataClientRpc()
+    {
+        if(puzzleData.connectedClients!=null)
+        {
+            SendPuzzleDataServerRpc(puzzleData.connectedClients.ToArray());
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void SendPuzzleDataServerRpc(ulong[] p)
+    {
+        SendPuzzleDataClientRpc(p);
+    }
+
+    [ClientRpc]
+    public void SendPuzzleDataClientRpc(ulong[] p)
+    {
+        puzzleData.connectedClients = p.ToList();
+        //Debug.Log("PuzzleDataUpdated");
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void KeyPressedServerRpc(int[] mazeLayouts)
+    {
+        KeyPressedClientRpc(mazeLayouts);
+    }
+
+    [ClientRpc]
+    public void KeyPressedClientRpc(int[] mazeLayouts)
+    {
+        
     }
 }
