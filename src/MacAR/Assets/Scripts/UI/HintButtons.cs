d35113d9aff1b52667a641386651ab59fd28119d
kannampe <kannampe@mcmaster.ca>
2024-03-20 03:28:17 -0400

    fixed hint system. resolved making button interactable for hint and text chat

--- src/MacAR/Assets/Scripts/UI/HintButtons.cs
+++ src/MacAR/Assets/Scripts/UI/HintButtons.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class HintButtons : MonoBehaviour
 {
-    public Text hintText;
     public Button forwardButton;
     public Button backButton;
     public Button dispButton;
+    public TextMeshProUGUI hintTexter;
+    public TextMeshProUGUI hintCounter;
     List<string> hintList = new List<string>();
     int counter = 0;
+    int hintCount = 0;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("got hint text");
-        Debug.Log(hintText.text);
         hintList.Add("This is the first hint");
         hintList.Add("This is the second hint");
         hintList.Add("This is the third hint");
+        hintTexter.text = "hello";
         Button forBtn = forwardButton.GetComponent<Button>();
         Button backBtn = backButton.GetComponent<Button>();
         Button dispBtn = dispButton.GetComponent<Button>();
@@ -29,7 +32,9 @@ public class HintButtons : MonoBehaviour
     void initialClick()
     {
         counter = 0;
-        hintText.text = hintList[counter];
+        hintTexter.text = hintList[counter];
+        hintCount = counter + 1;
+        hintCounter.text = " Hint: " + hintCount + "/" + hintList.Count;
         backButton.gameObject.SetActive(false);
         forwardButton.gameObject.SetActive(true);
         Debug.Log("initial" + counter);
@@ -38,6 +43,7 @@ public class HintButtons : MonoBehaviour
     void forwardClick()
     {
         counter += 1;
+        hintCount = counter + 1;
 
         if (counter > 0)
         {
@@ -54,7 +60,8 @@ public class HintButtons : MonoBehaviour
         }
 
         Debug.Log("counter in front" + counter);
-        hintText.text = hintList[counter];
+        hintTexter.text = hintList[counter];
+        hintCounter.text = " Hint: " + hintCount + "/" + hintList.Count;
 
 
 
@@ -66,6 +73,7 @@ public class HintButtons : MonoBehaviour
         if (counter > 0)
         {
             counter -= 1;
+            hintCount = counter + 1;
         }
 
         if (counter == 0)
@@ -78,7 +86,8 @@ public class HintButtons : MonoBehaviour
             forwardButton.gameObject.SetActive(true);
         }
         Debug.Log("counter in back" + counter);
-        hintText.text = hintList[counter];
+        hintTexter.text = hintList[counter];
+        hintCounter.text = " Hint: " + hintCount + "/" + hintList.Count;
 
     }
 
