22cb8803003dbf0ebe9a023406cc49a28930c855
Sam Gorman <gormansam8@gmail.com>
2024-03-23 13:45:40 -0400

    Implemented seed based wire generation

--- src/MacAR/Assets/Scripts/Puzzles/PuzzleBase.cs
+++ src/MacAR/Assets/Scripts/Puzzles/PuzzleBase.cs
@@ -5,6 +5,7 @@ using Unity.Netcode;
 
 public abstract class PuzzleBase : NetworkBehaviour
 {
+    public int seed;
     protected bool active = false;
     public abstract void InitializePuzzle();
     public virtual void SetActive(bool status)
